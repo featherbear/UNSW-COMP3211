537b83f76566ef65240c8f862d173e68f4d2f892
Andrew Wong <andrew.j.wong@outlook.com>
2021-03-10 04:45:26 +1100

    Add test bench

--- labs/lab2/task1/single_cycle_core/instruction_memory.vhd
+++ labs/lab2/task1/single_cycle_core/instruction_memory.vhd
@@ -46,17 +46,40 @@ begin
     begin
         if (reset = '1') then
 
+            -- :: python3 ::
+            -- toBin = lambda x: re.findall('....', bin(x)[2:].zfill(16))
+            -- toHex = lambda x: hex(int("".join(x), 2))
+            -- :::::::::::::
+            
             var_insn_mem(0)  := X"1010"; --  insn_0 : load  $1, $0, 0   - load data 0($0) into $1
             var_insn_mem(1)  := X"1021"; --  insn_1 : load  $2, $0, 1   - load data 1($0) into $2
             var_insn_mem(2)  := X"8013"; --  insn_2 : add   $3, $0, $1  - $3 <- $0 + $1
             var_insn_mem(3)  := X"8124"; --  insn_3 : add   $4, $1, $2  - $4 <- $1 + $2
             var_insn_mem(4)  := X"3032"; --  insn_4 : store $3, $0, 2   - store data $3 into 2($0)
             var_insn_mem(5)  := X"3043"; --  insn_5 : store $4, $0, 3   - store data $4 into 3($0)
-            var_insn_mem(6)  := X"0000"; --  
-            var_insn_mem(7)  := X"0000";
-            var_insn_mem(8)  := X"0000";
-            var_insn_mem(9)  := X"0000";
-            var_insn_mem(10) := X"0000";
+            
+            -- sll $5, $1, $3 ($5 = 5 << 5)
+            ---- toHex(['1100', '0001', '0011', '1001'])
+            var_insn_mem(6)  := X"C139";
+            
+            -- sll $6, $1, $2 ($5 = 5 << 8)
+            ---- toHex(['1100', '0001', '0011', '1010'])    
+            var_insn_mem(7)  := X"C13A";
+            
+            -- bne $2, $2, 14
+            ---- toHex(['1101', '0010', '0010', '1110'])
+            ---- shouldn't jump
+            var_insn_mem(8)  := X"D22E";
+            
+            -- bne $1, $3, 14
+            ---- toHex(['1101', '0001', '0011', '1110'])
+            ---- shouldn't jump
+            var_insn_mem(9)  := X"D13E";
+            
+            -- bne $2, $3, 14
+            ---- toHex(['1101', '0010', '0011', '1110'])
+            ---- shoul jump to instruction 14
+            var_insn_mem(10) := X"D23E";
             var_insn_mem(11) := X"0000";
             var_insn_mem(12) := X"0000";
             var_insn_mem(13) := X"0000";
