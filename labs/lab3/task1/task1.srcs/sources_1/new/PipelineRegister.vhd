8b73e64a895df5299a95b3eba8d3065f53a1c819
Andrew Wong <andrew.j.wong@outlook.com>
2021-03-30 01:52:31 +1100

    Add generic pipeline register that updates on falling edge

---
+++ labs/lab3/task1/task1.srcs/sources_1/new/PipelineRegister.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity PipelineRegister is
+    Generic ( s : INTEGER );
+    Port ( clk : in STD_LOGIC;
+           dIn : in STD_LOGIC_VECTOR(s-1 DOWNTO 0);
+           dOut : out STD_LOGIC_VECTOR(s-1 DOWNTO 0)
+         );
+end PipelineRegister;
+
+architecture Behavioural of PipelineRegister is
+begin
+    process (clk, dIn) begin
+        if (clk'event and falling_edge(clk)) then
+            dOut <= dIn;
+        end if;
+    end process;
+end Behavioural;
