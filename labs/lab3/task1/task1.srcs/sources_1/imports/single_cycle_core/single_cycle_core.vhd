e3b34c7913f6ba2278b06b78340bd0b27f815681
Andrew Wong <andrew.j.wong@outlook.com>
2021-03-31 04:35:34 +1100

    Assign signals

--- labs/lab3/task1/task1.srcs/sources_1/imports/single_cycle_core/single_cycle_core.vhd
+++ labs/lab3/task1/task1.srcs/sources_1/imports/single_cycle_core/single_cycle_core.vhd
@@ -1,9 +1,9 @@
 ---------------------------------------------------------------------------
--- single_cycle_core.vhd - A Single-Cycle Processor Implementation
+-- pipelined_core.vhd - A Single-Cycle Processor Implementation
 --
 -- Notes : 
 --
--- See single_cycle_core.pdf for the block diagram of this single
+-- See pipelined_core.pdf for the block diagram of this single
 -- cycle processor core.
 --
 -- Instruction Set Architecture (ISA) for the single-cycle-core:
@@ -54,12 +54,12 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity single_cycle_core is
+entity pipelined_core is
     port ( reset  : in  std_logic;
            clk    : in  std_logic );
-end single_cycle_core;
+end pipelined_core;
 
-architecture structural of single_cycle_core is
+architecture structural of pipelined_core is
 
 component program_counter is
     port ( reset    : in  std_logic;
@@ -158,6 +158,47 @@ signal sig_next_pc_standard     : std_logic_vector(3 downto 0);
 
 --
 
+signal sig_insn_src             : std_logic_vector(15 downto 0);
+
+signal sig_write_register_src   : std_logic_vector(3 downto 0);
+signal sig_write_register_src_ex_mem  : std_logic_vector(3 downto 0);
+signal sig_write_register_src_mem_wb  : std_logic_vector(3 downto 0);
+
+signal sig_mem_to_reg_src           : std_logic;
+signal sig_mem_to_reg_src_ex_mem           : std_logic;
+signal sig_mem_to_reg_src_mem_wb           : std_logic;
+
+signal sig_reg_write_src            : std_logic;
+signal sig_reg_write_src_ex_mem            : std_logic;
+signal sig_reg_write_src_mem_wb          : std_logic;
+
+signal sig_enable_jump_pc_src       : std_logic;
+signal sig_enable_jump_pc_src_ex_mem       : std_logic;
+
+signal sig_mem_write_src            : std_logic;
+signal sig_mem_write_src_ex_mem            : std_logic;
+
+signal sig_alu_src_src              : std_logic;
+
+signal sig_alu_operation_src        : std_logic;
+
+signal sig_read_data_a_src          : std_logic_vector(15 downto 0);
+signal sig_read_data_b_src          : std_logic_vector(15 downto 0);
+
+signal sig_sign_extended_offset_src : std_logic_vector(15 downto 0);
+
+signal sig_alu_carry_out_src        : std_logic;
+
+signal sig_alu_result_src           : std_logic_vector(15 downto 0); 
+
+signal sig_read_data_b_ex_mem          : std_logic_vector(15 downto 0);
+
+signal sig_alu_result_mem_wb           : std_logic_vector(15 downto 0); 
+
+signal sig_data_mem_out_src         : std_logic_vector(15 downto 0);
+
+--
+
 begin
 
     sig_one_4b <= "0001";
@@ -179,30 +220,30 @@ begin
     port map (
         mux_select => sig_use_jump_pc,
         data_a => sig_next_pc_standard,
-        data_b => sig_insn(3 downto 0),
-        data_out => sig_next_pc   
+        data_b => sig_insn_src(3 downto 0), --------------------------------- TODO: TEST if use _src or not, or some other value we haven't forwarded
+        data_out => sig_next_pc  
     );
     
     insn_mem : instruction_memory 
     port map ( reset    => reset,
                clk      => clk,
                addr_in  => sig_curr_pc,
-               insn_out => sig_insn );
+               insn_out => sig_insn_src );
 
     sign_extend : sign_extend_4to16 
     port map ( data_in  => sig_insn(3 downto 0),
-               data_out => sig_sign_extended_offset );
+               data_out => sig_sign_extended_offset_src );
 
     ctrl_unit : entity work.control_unit 
     port map ( opcode     => sig_insn(15 downto 12),
                reg_dst    => sig_reg_dst,
-               reg_write  => sig_reg_write,
-               alu_src    => sig_alu_src,
-               mem_write  => sig_mem_write,
-               mem_to_reg => sig_mem_to_reg,
+               reg_write  => sig_reg_write_src,
+               alu_src    => sig_alu_src_src,
+               mem_write  => sig_mem_write_src,
+               mem_to_reg => sig_mem_to_reg_src,
                --
-               alu_operation => sig_alu_operation,
-               enable_jump_pc => sig_enable_jump_pc
+               alu_operation => sig_alu_operation_src,
+               enable_jump_pc => sig_enable_jump_pc_src
                --
                );
 
@@ -210,7 +251,7 @@ begin
     port map ( mux_select => sig_reg_dst,
                data_a     => sig_insn(7 downto 4),
                data_b     => sig_insn(3 downto 0),
-               data_out   => sig_write_register );
+               data_out   => sig_write_register_src );
 
     reg_file : register_file 
     port map ( reset           => reset, 
@@ -220,8 +261,8 @@ begin
                write_enable    => sig_reg_write,
                write_register  => sig_write_register,
                write_data      => sig_write_data,
-               read_data_a     => sig_read_data_a,
-               read_data_b     => sig_read_data_b );
+               read_data_a     => sig_read_data_a_src,
+               read_data_b     => sig_read_data_b_src );
     
     mux_alu_src : mux_2to1_16b 
     port map ( mux_select => sig_alu_src,
@@ -233,8 +274,8 @@ begin
     port map ( 
                src_a     => sig_read_data_a,
                src_b     => sig_alu_src_b,
-               result    => sig_alu_result,
-               flag      => sig_alu_carry_out,
+               result    => sig_alu_result_src,
+               flag      => sig_alu_carry_out_src,
                ---
                operation => sig_alu_operation
                ---
@@ -244,14 +285,82 @@ begin
     port map ( reset        => reset,
                clk          => clk,
                write_enable => sig_mem_write,
-               write_data   => sig_read_data_b,
+               write_data   => sig_read_data_b_ex_mem,
                addr_in      => sig_alu_result(3 downto 0),
-               data_out     => sig_data_mem_out );
+               data_out     => sig_data_mem_out_src );
                
     mux_mem_to_reg : mux_2to1_16b 
     port map ( mux_select => sig_mem_to_reg,
-               data_a     => sig_alu_result,
+               data_a     => sig_alu_result_mem_wb,
                data_b     => sig_data_mem_out,
                data_out   => sig_write_data );
 
+    
+    pipeline_if_id: entity work.PipelineReg_IF_ID
+        port map (
+          clk => clk,
+          instrIn => sig_insn_src,
+          instr => sig_insn
+        );
+        
+    pipeline_id_ex: entity work.PipelineReg_ID_EX
+        port map (
+          clk => clk,
+          WBAddrIn => sig_write_register_src,
+          WBAddr => sig_write_register_src_ex_mem,
+          ctrl_MemToRegIN => sig_mem_to_reg_src,
+          ctrl_MemToReg => sig_mem_to_reg_src_ex_mem,
+          ctrl_RegWriteIN => sig_reg_write_src,
+          ctrl_RegWrite => sig_reg_write_src_ex_mem,
+          ctrl_EnableJumpPCIN => sig_enable_jump_pc_src ,
+          ctrl_EnableJumpPC => sig_enable_jump_pc_src_ex_mem      ,
+          ctrl_MemWriteIN => sig_mem_write_src ,
+          ctrl_MemWrite => sig_mem_write_src_ex_mem  ,
+          ctrl_ALUSrcIN => sig_alu_src_src,
+          ctrl_ALUSrc => sig_alu_src,
+          ctrl_ALUOperationIN => sig_alu_operation_src,
+          ctrl_ALUOperation => sig_alu_operation,
+          RegData1IN => sig_read_data_a_src,
+          RegData1 =>  sig_read_data_a,
+          RegData2IN => sig_read_data_b_src,
+          RegData2 =>  sig_read_data_b,
+          SignExtendDataIN => sig_sign_extended_offset_src,
+          SignExtendData => sig_sign_extended_offset
+        );
+        
+    pipeline_ex_mem: entity work.PipelineReg_EX_MEM
+        port map (
+          clk => clk,
+          WBAddrIn => sig_write_register_src_ex_mem,
+          WBAddr => sig_write_register_src_mem_wb,
+          ctrl_MemToRegIN => sig_mem_to_reg_src_ex_mem,
+          ctrl_MemToReg => sig_mem_to_reg_src_mem_wb,
+          ctrl_RegWriteIN => sig_reg_write_src_ex_mem,
+          ctrl_RegWrite => sig_reg_write_src_mem_wb,
+          ctrl_EnableJumpPCIN => sig_enable_jump_pc_src_ex_mem ,
+          ctrl_EnableJumpPC => sig_enable_jump_pc,
+          ctrl_MemWriteIN => sig_mem_write_src_ex_mem ,
+          ctrl_MemWrite => sig_mem_write,
+          ctrl_ALUFlagIN => sig_alu_carry_out_src,
+          ctrl_ALUFlag => sig_alu_carry_out,
+          ALUResultIN => sig_alu_result_src,
+          ALUResult => sig_alu_result,
+          dataMemoryWriteIN => sig_read_data_b,
+          dataMemoryWrite => sig_read_data_b_ex_mem
+        );
+        
+    pipeline_mem_wb: entity work.PipelineReg_MEM_WB
+        port map (
+          clk => clk,
+          WBAddrIn => sig_write_register_src_mem_wb,
+          WBAddr => sig_write_register,
+          ctrl_MemToRegIN => sig_mem_to_reg_src_mem_wb,
+          ctrl_MemToReg => sig_mem_to_reg,
+          ctrl_RegWriteIN => sig_reg_write_src_mem_wb,
+          ctrl_RegWrite => sig_reg_write_src,
+          ALUResultIN => sig_alu_result,
+          ALUResult => sig_alu_result_mem_wb,
+          dataMemoryIN => sig_data_mem_out_src,
+          dataMemory => sig_data_mem_out
+        );
 end structural;
--- labs/lab3/task1/task1.srcs/sources_1/new/PipelineReg_IF_ID.vhd
+++ labs/lab3/task1/task1.srcs/sources_1/new/PipelineReg_IF_ID.vhd
@@ -3,25 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity PipelineReg_IF_ID is
     Port ( clk : in STD_LOGIC;
-
-           -- BEGIN passthrough registers
-           ctrl_MemToRegIN  : in STD_LOGIC;
-           ctrl_MemToReg    : out STD_LOGIC;        
-           ctrl_RegWriteIN  : in STD_LOGIC;
-           ctrl_RegWrite    : out STD_LOGIC;
-           ctrl_EnableJumpPCIN : in STD_LOGIC;
-           ctrl_EnableJumpPC   : out STD_LOGIC;
-           ctrl_MemWriteIN  : in STD_LOGIC;
-           ctrl_MemWrite    : out STD_LOGIC;
-           ctrl_ALUSrcIN  : in STD_LOGIC;
-           ctrl_ALUSrc    : out STD_LOGIC;
-           ctrl_ALUOperationIN  : in STD_LOGIC;
-           ctrl_ALUOperation    : out STD_LOGIC;
-           -- END passthrough registers
-
-           addrIn  : in STD_LOGIC_VECTOR(3 DOWNTO 0);
-           addr    : out STD_LOGIC_VECTOR(3 DOWNTO 0);
-           
+          
            instrIn : in STD_LOGIC_VECTOR(15 DOWNTO 0);
            instr   : out STD_LOGIC_VECTOR(15 DOWNTO 0)
          );
@@ -31,26 +13,11 @@ architecture Behavioral of PipelineReg_IF_ID is
 
 begin
     reg: entity work.PipelineRegister
-        GENERIC MAP (n => 6 + 20)
+        GENERIC MAP (n => 16)
         PORT MAP (
             clk => clk,
-            
-            dIn(25) => ctrl_MemToRegIN,
-            dIn(24) => ctrl_RegWriteIN,
-            dIn(23) => ctrl_EnableJumpPCIN,
-            dIn(22) => ctrl_MemWriteIN,
-            dIn(21) => ctrl_ALUSrcIN,
-            dIn(20) => ctrl_ALUOperationIN,
-            dOut(25) => ctrl_MemToReg,
-            dOut(24) => ctrl_RegWrite,
-            dOut(23) => ctrl_EnableJumpPC,
-            dOut(22) => ctrl_MemWrite,
-            dOut(21) => ctrl_ALUSrc,
-            dOut(20) => ctrl_ALUOperation,
-             
-            dIn(19 DOWNTO 16) => addrIn,
+
             dIn(15 DOWNTO 0) => instrIn,
-            dOut(19 DOWNTO 16) => addr,
             dOut(15 DOWNTO 0) => instr
         );
 
