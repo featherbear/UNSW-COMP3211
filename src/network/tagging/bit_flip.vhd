0ae97c2f508a51283eaeda394d662675fb7ca763
Andrew Wong <andrew.j.wong@outlook.com>
2021-04-21 02:36:30 +1000

    Clean up bit_flip

--- src/network/tagging/bit_flip.vhd
+++ src/network/tagging/bit_flip.vhd
@@ -1,49 +1,15 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/12/2021 12:32:44 AM
--- Design Name: 
--- Module Name: bit_flip - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
--- feel like i'm missing something but simply inverts an 8-bit input if flip is set
-
 entity bit_flip is
-    Port ( data_in : in STD_LOGIC_VECTOR (7 downto 0);
-           data_out : out STD_LOGIC_VECTOR (7 downto 0);
-           flip : in STD_LOGIC);
+    port ( 
+        data_in  : in  STD_LOGIC_VECTOR (7 downto 0);
+        data_out : out STD_LOGIC_VECTOR (7 downto 0);
+        flip : in STD_LOGIC
+    );
 end bit_flip;
 
-architecture Behavioral of bit_flip is
-
+architecture Behavioural of bit_flip is
 begin
-
-    data_out <= not data_in when flip = '1' else
-                data_in;
-
-end Behavioral;
+    data_out <= not data_in when flip = '1' else data_in;
+end Behavioural;
